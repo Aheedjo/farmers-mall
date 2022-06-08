affc854dc3ac367503e4f2de6c72daf2b7a998d1
Imran Abdulmalik <isoteriksoftware@gmail.com>
2022-06-08 15:26:43 +0100

    Update products.js

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,7 +1,9 @@
-import { KeyboardArrowDownRounded, KeyboardArrowUpRounded } from '@mui/icons-material';
-import { Button, Menu, MenuItem, Typography } from '@mui/material';
+import { KeyboardArrowDownRounded, KeyboardArrowUpRounded, LocationOnRounded, MicRounded } from '@mui/icons-material';
+import { Button, Checkbox, Divider, FormControlLabel, FormGroup, Grid, IconButton, InputAdornment, Menu, MenuItem, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import { Form, Formik } from 'formik';
 import { useState } from 'react';
+import FormikField from '../components/FormikField';
 import Layout from '../components/Layout';
 
 const Root = styled('div')(({ theme }) => ({
@@ -10,10 +12,12 @@ const Root = styled('div')(({ theme }) => ({
 }));
 
 const Section1 = styled('div')(({ theme }) => ({
+  padding: '1rem 1rem',
+
   '& .header': {
     display: 'flex',
     justifyContent: 'space-between',
-    padding: '1rem 1rem',
+    marginBottom: '1.5rem',
 
     '& .title': {
       color: '#008036',
@@ -34,6 +38,53 @@ const Section1 = styled('div')(({ theme }) => ({
       padding: '.2rem .5rem',
     },
   },
+  '& .left': {
+    '& .title': {
+      color: theme.palette.primary.main,
+      fontWeight: 600,
+      marginBottom: '1rem',
+    }
+  },
+  '& .categories': {
+    '& .title': {
+      marginBottom: '1rem',
+    }
+  },
+  '& .location': {
+    marginTop: '2rem',
+
+    '& .searchContainer': {
+      width: '100%',
+      paddingRight: '2rem',
+
+      '& .field': {
+        width: '100%',
+        '& .MuiOutlinedInput-root': {
+          width: '100%',
+
+          '& fieldset': {
+            borderColor: theme.palette.primary.main,
+          },
+          '&:hover fieldset': {
+            borderColor: theme.palette.primary.main,
+          },
+          '&.Mui-focused fieldset': {
+            borderColor: theme.palette.primary.main,
+          },
+        },
+      },
+      '& .field input': {
+        padding: '1rem',
+        width: '100%'
+      },
+      '& .startAdornment': {
+        '&.MuiInputAdornment-root': {
+          margin: 0,
+          padding: 0,
+        }
+      }
+    }
+  }
 }));
 
 const SortDropdown = () => {
@@ -76,6 +127,60 @@ const SortDropdown = () => {
   );
 };
 
+const CategoryCheck = ({ label, defaultChecked = false }) => {
+  return (
+    <FormControlLabel control={<Checkbox defaultChecked={defaultChecked} />} label={label} />
+  );
+};
+
+const SearchButtons = () => {
+  const Root = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+
+    '& .searchBtn': {
+      color: theme.colors.primaryVariant,
+      background: '#E0FFDD',
+      padding: '.2rem',
+
+      '& .icon': {
+        fontSize: '1.5rem'
+      }
+    }
+  }));
+
+  return (
+    <Root>
+      <IconButton className="searchBtn">
+        <MicRounded className='icon'/>
+      </IconButton>
+    </Root>
+  );
+};
+
+const LocationIcon = () => {
+  const Root = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+
+    '& .searchBtn': {
+      color: theme.colors.primaryVariant,
+      
+      '& .icon': {
+        fontSize: '1.5rem'
+      }
+    }
+  }));
+
+  return (
+    <Root>
+      <IconButton className="searchBtn">
+        <LocationOnRounded className='icon'/>
+      </IconButton>
+    </Root>
+  );
+};
+
 const Products = () => {
   return (
     <Layout>
@@ -90,6 +195,54 @@ const Products = () => {
               <SortDropdown/>
             </div>
           </div>
+
+          <Grid container>
+            <Grid item md={3} className='left'>
+              <div className='categories'>
+                <Typography className='title' variant="body1">Categories</Typography>
+                <FormGroup>
+                  <CategoryCheck label='Spices and Vegetables'/>
+                  <CategoryCheck label='Fruit Crops' defaultChecked/>
+                  <CategoryCheck label='Oil Crops and Legumes'/>
+                  <CategoryCheck label='Beverages'/>
+                  <CategoryCheck label='Root and Tuber'/>
+                  <CategoryCheck label='Cereals and grains'/>
+                  <CategoryCheck label='Fisheries and aquaculture'/>
+                  <CategoryCheck label='Others'/>
+                </FormGroup>
+              </div>
+
+              <Divider style={{ background: '#008036', height: '1.4px', margin: '1rem 0'}}/>
+
+              <div className='location'>
+                <Typography className='title' variant="body1">Location</Typography>
+
+                <div className="searchContainer">
+                <Formik
+                  initialValues={{
+                    search: ''
+                  }}
+                >
+                  <Form>
+                    <FormikField
+                      name="search"
+                      variant="outlined"
+                      color="primary"
+                      className="field"
+                      InputProps={{ 
+                        startAdornment: <InputAdornment className="startAdornment" position="start"><LocationIcon/></InputAdornment>,
+                        endAdornment: <InputAdornment className="startAdornment" position="end"><SearchButtons/></InputAdornment>
+                      }}
+                      inputProps={{
+                        placeholder: 'Enter Location'
+                      }}
+                    />
+                  </Form>
+                </Formik>
+              </div>
+              </div>
+            </Grid>
+          </Grid>
         </Section1>
       </Root>
     </Layout>
